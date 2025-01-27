128d1ae0102c1847b470d18785c13c1136ca5efb
Brent Crowley <brent.crowley@neds.com>
2025-01-27 16:01:33 +1100

    navigation - add new project

---
+++ Navigation/Navigation/Navigation/NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationApp.swift
+//  Navigation
+//
+//  Created by Brent Crowley on 27/1/2025.
+//
+
+import SwiftUI
+
+@main
+struct NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
